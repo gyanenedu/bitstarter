e710f8088eca18bc074180506ae24c19bb1379fd
gyanenedu <gyan107@gmail.com>
2013-07-15 01:42:34 +0530

    Updated  again web.js

--- web.js
+++ web.js
@@ -1,20 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var fs= require('fs');
-buf = new Buffer(256);
 
-buf.write(fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-}));
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8'));
+    var k = fs.readFileSync('./index.html','utf8');
+    response.send( k.toString() );
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
